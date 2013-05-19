48b7f85e859004ef3aa3e50219ec7cb6f546cf41
Saeid Hatami <hatami4510@gmail.com>
2013-05-19 19:09:45 +0330

    add files (admin-panel)

---
+++ admin-panel/adminpanel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title> پانل مدیریت</title>
+	<link rel="stylesheet" type="text/css" href="1styles.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	
+	<script type="text/javascript" src="js/jquery.js"></script>	
+	<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+    <header>
+      <a href="#" class="logo">Media Teq Admin Panel</a>
+      <div id="mini-nav">
+        <ul class="hidden-phone">
+          <li><a href="#" >وظایف</a></li>
+          <li><a href="#">ثبت نام ها <span id="newSignup">06</span></a></li>         
+          <li><a href="login.html">خروج</a></li>
+        </ul>
+      </div>
+    </header>
+ <!-- <div id="top" class= "admin_top">top</div> -->
+ <div id="clear" class="badboy"> </div>
+ <div id="right" class="admin_right_panel"> right </div> 
+ <div id="" class="admin_container">container </div>
+ </body>
+</html>
