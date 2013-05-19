c8992b55c3c497d335fdcfe3af67ded9235f2e1c
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-05-19 18:05:41 +0330

    replace code to header.php

---
+++ main-site/inc/header.php
@@ -0,0 +1,46 @@
+<header>
+	<div class="top">
+		<div class="time right"><p>شنبه ، 28 اردیبهشت 1392</p></div>
+		<div class="search left">
+			<form action="">
+				<input type="text" name="search" class="search right" value="جستجو..." />
+				<input type="submit" name="submit" class="submit left" value="" />
+				<span class="left"></span>
+			</form>
+			<div class="badboy"></div>
+		</div>
+		<div class="menu">
+			<menu>
+				<li><a href="#">صفحه اصلی</a></li>
+				<li><a href="#">درباره ما</a></li>
+				<li><a href="#">گالری تصاویر</a></li>
+				<li><a href="#">خدمات</a></li>
+				<li><a href="#">تماس با ما</a></li>
+			</menu>
+		</div>
+	</div>
+	<div class="badboy"></div>
+	<div class="mid">
+		<div class="logo left">
+			<h2>
+				<a href="#" title="مدیا تک">
+					<img src="./images/logo.png" alt="مدیا تک">
+					<strong>Mediateq....</strong>
+				</a>
+			</h2>
+		</div>
+		<div class="ads right">
+			<a href="#"><img src="./images/others/ads.jpg"></a>
+		</div>
+	</div>
+	<div class="badboy"></div>
+	<div class="main-menu">
+		<menu>
+			<li class="active"><a href="#"></a></li>
+			<li><a href="#">درباره ما</a></li>
+			<li><a href="#">گالری تصاویر</a></li>
+			<li><a href="#">خدمات</a></li>
+			<li><a href="#">تماس با ما</a></li>
+		</menu>
+	</div>
+</header>
