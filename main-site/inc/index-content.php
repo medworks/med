2ad301aac78bc3188b378616e39803721ff2c464
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-05-21 11:18:20 +0330

    separate codes

---
+++ main-site/inc/index-content.php
@@ -0,0 +1,79 @@
+<section class="main-content">
+	<div class="content right">
+		<div class="slideshow box">
+			<ul>
+				<li>
+					<img src="./images/others/slide1.jpg" alt="">
+				</li>
+			</ul>
+		</div>
+		<div class="badboy"></div>
+		<div class="recent-works main-box">
+			<h2>کارهای اَخیر</h2>
+			<div class="line"></div>
+			<div class="badboy"></div>
+			<div class="works box-right">
+				<ul>
+					<li>
+						<a href="#"><img src="./images/others/works.jpg" alt=""></a>
+						<h3>توضیحات...</h3>
+						<p>21 اردیبهشت 1392</p>
+					</li>
+					<li>
+						<a href="#"><img src="./images/others/works.jpg" alt=""></a>
+						<h3>توضیحات...</h3>
+						<p>21 اردیبهشت 1392</p>
+					</li>
+					<li>
+						<a href="#"><img src="./images/others/works.jpg" alt=""></a>
+						<h3>توضیحات...</h3>
+						<p>21 اردیبهشت 1392</p>
+					</li>
+				</ul>
+				<div class="badboy"></div>
+			</div>
+		</div>
+	</div>
+	<div class="sidebar right">
+		<div class="social box">
+			<ul>
+				<li>
+					<a href="#" class="dribble"></a>
+				</li>
+				<li>
+					<a href="#" class="facebook"></a>
+				</li>
+				<li>
+					<a href="#" class="twitter"></a>
+				</li>
+				<li>
+					<a href="#" class="vimeo"></a>
+				</li>
+				<li>
+					<a href="#" class="youtube"></a>
+				</li>
+				<li>
+					<a href="#" class="rss"></a>
+				</li>
+			</ul>
+			<div class="badboy"></div>
+		</div>
+		<div class="login-panel main-box">
+			<h2>ورود کاربران</h2>
+			<div class="line"></div>
+			<div class="badboy"></div>
+			<div class="login box-left">
+				<form action="">
+					<p><input type="text" name="username" class="username" value="نام کاربری" onfocus="if (this.value == 'نام کاربری') {this.value = '';}" onblur="if (this.value == '') {this.value = 'نام کاربری';}" /></p>
+					<p><input type="password" name="password" class="password" value="رمز عبور" onfocus="if (this.value == 'رمز عبور') {this.value = '';}" onblur="if (this.value == '') {this.value = 'رمز عبور';}" /></p>
+					<p class="right"><input type="submit" name="submit" class="submit" value="ورود" /></p>
+					<label class="right"><input type="checkbox" checked="checked" name="remember" class="remember"/> به خاطر بسپار</label>
+				</form>
+				<div class="badboy"></div>
+				<a href="#" class="forget">رمز خود را فراموش کرده ام!</a>
+				<div class="badboy"></div>
+			</div>
+		</div>
+	</div>
+	<div class="badboy"></div>
+</section>
