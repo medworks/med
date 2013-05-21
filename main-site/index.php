100a8c667a3659d3d6a7e64863b89d5c781db358
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-05-21 13:19:27 +0330

    fix footer

--- main-site/index.php
+++ main-site/index.php
@@ -15,9 +15,103 @@
 			include("./inc/bot-ads.php");
 		?>
 
-		<footer>
-			
-		</footer>
 	</div>
+	<footer>
+		<div class="container">
+			<div class="first">
+				<div class="title"><h4>اخبار</h4></div>
+				<div class="content">
+					<ul>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+					</ul>
+					<div class="badboy"></div>
+				</div>
+			</div>
+			<div class="second">
+				<div class="title"><h4>گالری تصاویر</h4></div>
+				<div class="content">
+					<ul>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+					</ul>
+					<div class="badboy"></div>
+				</div>
+			</div>
+			<div class="third">
+				<div class="title"><h4>پروژهای اخیر</h4></div>
+				<div class="content">
+					<ul>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+					</ul>
+					<div class="badboy"></div>
+				</div>
+			</div>
+			<div class="forth">
+				<div class="title"><h4>آخرین پست ها</h4></div>
+				<div class="content">
+					<ul>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+						<li>
+							<div class="pic"><a href="#"><img src="./images/others/works.jpg" alt=""></a></div>
+							<h3><a href="#">جزئیات...  جزئیات... جزئیات... جزئیات...</a></h3>
+							<span class="date">31 اردیبهشت 1392</span>
+						</li>
+					</ul>
+					<div class="badboy"></div>
+				</div>
+			</div>
+			<div class="badboy"></div>
+		</div>
+	</footer>
 </body>
 </html>
