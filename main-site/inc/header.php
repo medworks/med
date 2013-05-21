ef88ff2e304d496659d6977bfa755ce5454d1d66
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-05-21 11:49:29 +0330

    add header part to header file

--- main-site/inc/header.php
+++ main-site/inc/header.php
@@ -1,46 +1,62 @@
-<header>
-	<div class="top">
-		<div class="time right"><p>شنبه ، 28 اردیبهشت 1392</p></div>
-		<div class="search left">
-			<form action="">
-				<input type="text" name="search" class="search right" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}" />
-				<input type="submit" name="submit" class="submit left" value="" />
-				<span class="left"></span>
-			</form>
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="./1styles.css" />
+	<link rel="stylesheet" href="./style.css" />
+	<script src="./js/jquery.js" type="text/javascript"></script>
+	<script src="./js/script.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="./js/html5shiv.js"></script>
+	<![endif]-->
+	<link rel="Shortcut Icon" href="./favicon.ico" />
+</head>
+<body>
+	<div class="container">
+		<header>
+			<div class="top">
+				<div class="time right"><p>شنبه ، 28 اردیبهشت 1392</p></div>
+				<div class="search left">
+					<form action="">
+						<input type="text" name="search" class="search right" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}" />
+						<input type="submit" name="submit" class="submit left" value="" />
+						<span class="left"></span>
+					</form>
+					<div class="badboy"></div>
+				</div>
+				<div class="menu">
+					<menu>
+						<li><a href="#">صفحه اصلی</a></li>
+						<li><a href="#">درباره ما</a></li>
+						<li><a href="#">گالری تصاویر</a></li>
+						<li><a href="#">خدمات</a></li>
+						<li><a href="#">تماس با ما</a></li>
+					</menu>
+				</div>
+			</div>
 			<div class="badboy"></div>
-		</div>
-		<div class="menu">
-			<menu>
-				<li><a href="#">صفحه اصلی</a></li>
-				<li><a href="#">درباره ما</a></li>
-				<li><a href="#">گالری تصاویر</a></li>
-				<li><a href="#">خدمات</a></li>
-				<li><a href="#">تماس با ما</a></li>
-			</menu>
-		</div>
-	</div>
-	<div class="badboy"></div>
-	<div class="mid">
-		<div class="logo left">
-			<h2>
-				<a href="#" title="مدیا تک">
-					<img src="./images/logo.png" alt="مدیا تک">
-					<strong>Mediateq....</strong>
-				</a>
-			</h2>
-		</div>
-		<div class="ads right">
-			<a href="#"><img src="./images/others/ads.jpg"></a>
-		</div>
-	</div>
-	<div class="badboy"></div>
-	<div class="main-menu">
-		<menu>
-			<li class="active"><a href="#"></a></li>
-			<li><a href="#">درباره ما</a></li>
-			<li><a href="#">گالری تصاویر</a></li>
-			<li><a href="#">خدمات</a></li>
-			<li><a href="#">تماس با ما</a></li>
-		</menu>
-	</div>
-</header>
+			<div class="mid">
+				<div class="logo left">
+					<h2>
+						<a href="#" title="مدیا تک">
+							<img src="./images/logo.png" alt="مدیا تک">
+							<strong>Mediateq....</strong>
+						</a>
+					</h2>
+				</div>
+				<div class="ads right">
+					<a href="#"><img src="./images/others/ads.jpg"></a>
+				</div>
+			</div>
+			<div class="badboy"></div>
+			<div class="main-menu">
+				<menu>
+					<li class="active"><a href="#"></a></li>
+					<li><a href="#">درباره ما</a></li>
+					<li><a href="#">گالری تصاویر</a></li>
+					<li><a href="#">خدمات</a></li>
+					<li><a href="#">تماس با ما</a></li>
+				</menu>
+			</div>
+		</header>
