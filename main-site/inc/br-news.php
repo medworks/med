7a5b1e7b73f610eff74fce5884381b3530286517
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-05-27 10:24:00 +0330

    add br-news to header

--- main-site/inc/br-news.php
+++
@@ -1,9 +0,0 @@
-<section class="br-news">
-	<span class="right">خبرهای فوری</span>
-	<div class="news right">
-		<ul>
-			<a href="#">خبرهای فوری...</a>
-		</ul>
-	</div>
-	<div class="badboy"></div>
-</section>
