fc5ae8459874a8372ac0008ee657fc43ac4e97c6
Saeid Hatami <hatami4510@gmail.com>
2013-05-28 21:04:25 +0330

    changing uploaded file name

--- config.php
+++ config.php
@@ -11,4 +11,5 @@
   define("_ADMIN", 1);
   define("_USER", 2);
   define("_ANONYMOUS", 3);
+  define ('SITE_ROOT', realpath(dirname(__FILE__)));
   ?>
--- manager/worksmgr.php
+++ manager/worksmgr.php
@@ -9,15 +9,18 @@
      //echo "enter to file if<br/>",
      $filename =strtolower(basename($_FILES['pic']['name']));
      $ext = substr($filename, strrpos($filename, '.') + 1);
-     // $newfilename= md5(rand() * time());
+     //$newfilename= md5(rand() * time());
+	 $newfilename = $_POST['subject'];
+	 
+	 $ext=".".$ext;
      //Determine the path to which we want to save this file
-    // $ext=".".$ext;
-     $newfilename = $_FILES['pic']['name'];
-     $newname = $_SERVER['DOCUMENT_ROOT'].'/workspics/'.$_FILES['pic']['name'];//$newfilename.$ext;
-     if (move_uploaded_file($_FILES['pic']['tmp_name'],$newname))
+     
+     //$newfilename = $_FILES['pic']['name'];
+     $newname = SITE_ROOT.'/workspics/'.$newfilename.$ext;
+     if (move_uploaded_file($_FILES["pic"]["tmp_name"],$newname))
      {       
        //echo("عمليات آپلود با مشكل مواجه شد");      
-     }
+     }	 
    }     
 	$fields = array("`subject`","`image`","`body`","`sdate`","`fdate`");
 	$values = array("'{$_POST[subject]}'","'{$newname}'","'{$_POST[detail]}'","'{$_POST[sdate]}'","'{$_POST[fdate]}'");
