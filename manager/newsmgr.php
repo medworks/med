4612ea0f63d3b889c41343ebc0882883819b86cd
Saeid Hatami <hatami4510@gmail.com>
2013-06-04 21:44:04 +0330

    delete un valuble code

--- manager/newsmgr.php
+++ manager/newsmgr.php
@@ -179,7 +179,6 @@ $html=<<<cd
 		theme : "advanced",
 		skin : "o2k7",
 		plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
-
 		// Theme options
 		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
 		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
@@ -188,17 +187,7 @@ $html=<<<cd
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true,
-
-		// Example content CSS (should be your site CSS)
-		content_css : "tiny_mce/css/content.css",
-
-		// Drop lists for link/image/media/template dialogs
-		template_external_list_url : "tiny_mce/lists/template_list.js",
-		external_link_list_url : "tiny_mce/lists/link_list.js",
-		external_image_list_url : "tiny_mce/lists/image_list.js",
-		media_external_list_url : "tiny_mce/lists/media_list.js",
-
+		theme_advanced_resizing : true,	
 		// Style formats
 		style_formats : [
 			{title : 'Bold text', inline : 'b'},
@@ -214,6 +203,7 @@ $html=<<<cd
 		template_replace_values : {
 			username : "Some User",
 			staffid : "991234"
+		
 		}
 	});
 </script>
