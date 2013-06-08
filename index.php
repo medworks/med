8737699489b2fa194424d884ebc833eaa10db27c
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-06-08 17:04:30 +0330

    add link

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
     include_once("./classes/functions.php");
 	// include_once("./themes/default/main.php");
 	// include_once("./manager/adminpanel.php");
-	include_once("./manager/adminlogin.php");
+	// include_once("./manager/adminlogin.php");
+	include_once("./themes/default/single-works.php");
 ?>
