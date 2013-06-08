be1f12d2bc60a84a7a84774f7ae1f3cd3613c5f8
Saeid Hatami <hatami4510@gmail.com>
2013-06-08 21:12:21 +0330

    commit my job

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	error_reporting (E_ALL ^ E_NOTICE);
     include_once("./classes/functions.php");
-	include_once("./themes/default/main.php");
+	//include_once("./themes/default/main.php");
 	// include_once("./manager/adminpanel.php");
-	// include_once("./manager/adminlogin.php");
+	 include_once("./manager/adminlogin.php");
 ?>
