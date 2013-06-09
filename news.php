075d8f94d049b0d807976af8d8ed0e718fe9388f
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-06-09 12:41:22 +0330

    fix date in news for persian

--- news.php
+++ news.php
@@ -1,6 +1,7 @@
 <?php
   include_once("./config.php");
   include_once("./classes/database.php");
+  include_once("./lib/persiandate.php");
   $db = database::getDatabase();
   $news = $db->SelectAll('news',NULL,NULL," ndate DESC");
   $html = " <div class='content'>
@@ -10,25 +11,26 @@
                 <div class='badboy'></div>
                 ";
 
-  for($i=0;$i<count($news);$i++)
-  {
+  for($i=0;$i<count($news);$i++){
+     $ndate = ToJalali($news["ndate"]," l d F  Y ");
   $html.=<<<cd
     <div class='box-right'> 
   	  <div class='title'>
-        <a href="#" title='{$news[$i]["subject"]}'><p>{$news[$i]["subject"]}</p></a>
+        <a href="?item=fullnews&act=do&wid={$news[$i]["id"]}" title='{$news[$i]["subject"]}'><p>{$news[$i]["subject"]}</p></a>
       </div>
       <div class='time'>
-        <p><span>تاریخ ثبت:</span> {$news[$i]["ndate"]}</p>  
+        <p><span>تاریخ ثبت:</span> {$ndate}</p>  
         <p><span>توسط:</span> {$news[$i]["userid"]} </p>
         <p><span>منبع:</span> {$news[$i]["resource"]}</p>  
       </div>
       <div class='badboy'></div>
       <div class="pic">
-        <a href="#" title='{$news[$i]["subject"]}'><img src='{$news[$i]["image"]}' alt='{$news[$i]["subject"]}'></a>
+        <a href="?item=fullnews&act=do&wid={$news[$i]["id"]}" title='{$news[$i]["subject"]}'><img src='{$news[$i]["image"]}' alt='{$news[$i]["subject"]}'></a>
       </div>
   		<div class="detail">
   			{$news[$i]["body"]}
   		</div>
+      <a href="?item=fullnews&act=do&wid={$news[$i]["id"]}" title="" class="button">توضیحات بیشتر</a>
       <div class='badboy'></div>
     </div>
 cd;
