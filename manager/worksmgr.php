1ae602e89d480ec3dc257c2a5ab23cbbe5356a4e
Saeid Hatami <hatami4510@gmail.com>
2013-06-14 12:22:37 +0330

    completing worksmgr as newsmgr

--- manager/worksmgr.php
+++ manager/worksmgr.php
@@ -4,7 +4,8 @@
 	include_once("../classes/messages.php");
 	include_once("../classes/session.php");	
 	include_once("../classes/functions.php");
-	//$sess = Session::getSesstion();
+	include_once("../lib/persiandate.php");	
+	$db = Database::GetDatabase();
     if ($_POST["mark"]=="saveworks")
 	{
 		$db = Database::GetDatabase();
@@ -216,5 +217,154 @@ if ($_GET['act']=="new" or $_GET['act']=="edit")
 	<!-- /TinyMCE -->  
 cd;
   }
+else
+if ($_GET['act']=="mgr")
+{
+	if ($_POST["mark"]=="srhnews")
+	{	 		
+	    if ($_POST["cbsearch"]=="ndate")
+		{
+		   date_default_timezone_set('Asia/Tehran');		   
+		   list($year,$month,$day) = explode("/", trim($_POST["txtsrh"]));		
+		   list($gyear,$gmonth,$gday) = jalali_to_gregorian($year,$month,$day);		
+		   $_POST["txtsrh"] = Date("Y-m-d",mktime(0, 0, 0, $gmonth, $gday, $gyear));
+		}
+		$rows = $db->SelectAll(
+				"works",
+				"*",
+				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
+				"ndate DESC",
+				$_GET["pageNo"]*10,
+				10);
+			if (!$rows) 
+			{					
+				$_GET['item'] = "worksmgr";
+				$_GET['act'] = "mgr";
+				$_GET['msg'] = 6;				
+				//header("Location:?item=worksmgr&act=mgr&msg=6");
+			}
+		
+	}
+	else
+	{	
+		$rows = $db->SelectAll(
+				"works",
+				"*",
+				null,
+				"ndate DESC",
+				$_GET["pageNo"]*10,
+				10);
+    }
+                $rowsClass = array();
+                $colsClass = array();
+                $rowCount =($_GET["rec"]=="all")?$db->CountAll("works"):Count($rows);
+                for($i = 0; $i < Count($rows); $i++)
+                {						
+		        $rows[$i]["subject"] =(mb_strlen($rows[$i]["subject"])>20)?mb_substr($rows[$i]["subject"],0,20,"UTF-8")."...":$rows[$i]["subject"];
+                $rows[$i]["body"] =(mb_strlen($rows[$i]["body"])>30)?
+                mb_substr(html_entity_decode(strip_tags($rows[$i]["body"]), ENT_QUOTES, "UTF-8"), 0, 30,"UTF-8") . "..." :
+                html_entity_decode(strip_tags($rows[$i]["body"]), ENT_QUOTES, "UTF-8");               
+                $rows[$i]["sdate"] =ToJalali($rows[$i]["sdate"]," l d F  Y ");
+				$rows[$i]["fdate"] =ToJalali($rows[$i]["fdate"]," l d F  Y ");                
+				if ($i % 2==0)
+				 {
+						$rowsClass[] = "datagridevenrow";
+				 }
+				else
+				{
+						$rowsClass[] = "datagridoddrow";
+				}
+				$rows[$i]["username"]=GetUserName($rows[$i]["userid"]); 
+				$rows[$i]["edit"] = "<a href='?item=worksmgr&act=edit&nid={$rows[$i]["id"]}' " .
+						"style='text-decoration:none;'><img src='../themes/default/images/admin/icons/edit.gif'></a>";								
+				$rows[$i]["delete"]=<<< del
+				<a href="javascript:void(0)"
+				onclick="DelMsg('{$rows[$i]['id']}',
+					'از حذف این فعالیت اطمینان دارید؟',
+				'?item=worksmgr&act=del&pageNo={$_GET[pageNo]}&nid=');"
+				 style='text-decoration:none;'><img src='../themes/default/images/admin/icons/delete.gif'></a>
+del;
+                         }
+
+    if (!$_GET["pageNo"] or $_GET["pageNo"]<=0) $_GET["pageNo"] = 0;
+            if (Count($rows) > 0)
+            {                    
+                    $gridcode .= DataGrid(array( 
+							"subject"=>"عنوان",
+							"body"=>"توضیحات",
+							"sdate"=>"تاریخ شروع",
+							"fdate"=>"تاریخ پایان",							
+                            "edit"=>"ویرایش",
+							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=worksmgr&act=mgr");
+                    
+            }
+$msgs = GetMessage($_GET['msg']);
+$list = array("subject"=>"عنوان",
+              "body"=>"توضیحات",
+			  "sdate"=>"تاریخ شروع",
+			  "fdate"=>"تاریخ پایان");
+$combobox = SelectOptionTag("cbsearch",$list,"subject");
+$code=<<<edit
+<script type='text/javascript'>
+	$(document).ready(function(){	   			
+		$('#srhsubmit').click(function(){	
+			$('#frmsrh').submit();
+			return false;
+		});
+		$('#cbsearch').change(function(){
+			$("select option:selected").each(function(){
+	            if($(this).val()=="ndate"){
+	            	$('.cal-btn').css('display' , 'inline-block');
+	            	return false;
+	            }else{
+	            	$('.cal-btn').css('display' , 'none');
+	            }
+  			});
+		});
+	});
+</script>	   
+					<div class="title">
+				      <ul>
+				        <li><a href="adminpanel.php">پیشخوان</a></li>
+					    <li><span>مدیریت کار ها</span></li>
+				      </ul>
+				      <div class="badboy"></div>
+				  </div>
+                    <div class="Top">                       
+						<center>
+							<form action="?item=worksmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+								<p>جستجو بر اساس {$combobox}</p>
+
+								<p class="search-form">
+									<input type="text" id="date_input_1" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  /> 
+									<img src="../themes/default/images/admin/cal.png" class="cal-btn" id="date_btn_2" alt="cal-pic">
+							         <script type="text/javascript">
+							          Calendar.setup({
+							            inputField  : "date_input_1",   // id of the input field
+							            button      : "date_btn_2",   // trigger for the calendar (button ID)
+							                ifFormat    : "%Y/%m/%d",       // format of the input field
+							                showsTime   : false,
+							                dateType  : 'jalali',
+							                showOthers  : true,
+							                langNumbers : true,
+							                weekNumbers : true
+							          });
+							        </script>
+									<a href="?item=worksmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=worksmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+								</p>
+								<input type="hidden" name="mark" value="srhnews" /> 
+								{$msgs}
+
+								{$gridcode} 
+															
+							</form>
+					   </center>
+					</div>
+
+edit;
+$html = $code;
+}	
 return $html;
 ?>
