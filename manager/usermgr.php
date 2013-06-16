ab9218e156c5a8f663f0100ae898d61b737322f2
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-06-16 13:22:21 +0330

    add validate for file

--- manager/usermgr.php
+++ manager/usermgr.php
@@ -94,7 +94,7 @@ $html=<<<cd
     	   <label for="pic">عکس </label>
          <span>*</span>
        </p>
-       <input type="file" name="pic" class="pic" id="pic" />
+       <input type="file" name="pic" class="validate[required] pic" id="pic" />
        <p>
          <label for="email">ایمیل </label>
          <span>*</span>
