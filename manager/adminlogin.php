77077c074c28274027be395c314dab51aeb22b26
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-06-19 19:42:27 +0330

    change style

--- manager/adminlogin.php
+++ manager/adminlogin.php
@@ -37,22 +37,27 @@ else
 	</head>
 	<body>
 	<div class="container">
-		<form action="" method="post">
-				<fieldset>
-						<legend>Log in</legend>			
-						<label for="user">نام کاربری :</label>
-						<input type="text" id="user" name="username"/>			
-						<label for="password">رمز عبور :</label>
-						<input type="password" id="password" name="password"/>			
-						<input type="submit"  class="button" name="login" value="ورود"/>
-						<input type="hidden" name="mark" value="adminlogin" />    
-				</fieldset>
-		</form>
-		<div class="badboy"></div>
-		<div class="mes">
-			{$adminloginmsg}
+		<div class="logo"><img src="../themes/default/images/admin/logo.png" alt="mediateq"></div>
+		<div class="login-content">
+			<form action="" method="post">
+					<fieldset>
+							<legend>Log in</legend>			
+							<label for="user">نام کاربری :</label>
+							<input type="text" id="user" class="ltr" name="username"/>			
+							<label for="password">رمز عبور :</label>
+							<input type="password" id="password" class="ltr" name="password"/>			
+							<input type="submit"  class="button" name="login" value="ورود"/>
+							<input type="hidden" name="mark" value="adminlogin" />    
+					</fieldset>
+			</form>
+			<div class="badboy"></div>
+			<div class="mes">
+				{$adminloginmsg}
+			</div>
 		</div>
 	</div>
+	<div class="top-login"></div>
+	<div class="top-login2"></div>
 	</body>
 	</html>
 cod;
