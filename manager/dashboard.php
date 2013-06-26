f44a600be7cb4d76fd60cbad7d20dd51868b8d4c
Saeid Hatami <hatami4510@gmail.com>
2013-06-26 21:08:49 +0330

    fixing errors of charts

--- manager/dashboard.php
+++ manager/dashboard.php
@@ -6,27 +6,41 @@
 		
 	$db = Database::GetDatabase();
 	$news = $db->SelectAll("news","*",null,"ndate");
+	$works = $db->SelectAll("works","*",null,"fdate");
 	$row = array();
-	$gdate = array();
+	$itemcount = array();
 	$count = array();
 	foreach($news as $key => $val)
 	{
-	  $row[] = "'".ToJalali($val['ndate'],"Y-m-d")."'";
-	  $gdate[] = "'".date("Y-m-d",strtotime($val['ndate']))."'";	  
-	}
-	//echo "count gdate is :",count($gdate);
-	//foreach($gdate as $key => $val) echo $val."<br/>";
-	$uniq = array_unique($row);
-	$guniq = array_unique($gdate);
-	//echo "count guniq is :",count($guniq);
-	foreach($gdate as $key => $val)
+	  $row[] = "'".ToJalali($val['ndate'],"Y-m-d")."'";	
+	}	
+	$uniq = array_unique($row);	
+	$itemcount = array_count_values($row);	
+	foreach($itemcount as $key => $val)
 	{
-		$count[] = $db->CountOf("news","ndate = {$val}");
-        //echo $db->cmd;		
+		$count[] = $val;        
 	}
-	$xAxis = implode(', ',$uniq);
-	$series = implode(', ',$count);
-
+	$xnAxis = implode(', ',$uniq);
+	$nseries = implode(', ',$count);
+	unset($row);
+	unset($itemcount);
+	unset($count);
+//*************************************************************
+    foreach($works as $key => $val)
+	{
+	  $row[] = "'".ToJalali($val['fdate'],"Y-m-d")."'";	
+	}	
+	$uniq = array_unique($row);	
+	$itemcount = array_count_values($row);	
+	foreach($itemcount as $key => $val)
+	{
+		$count[] = $val;        
+	}
+	$xwAxis = implode(', ',$uniq);
+	$wseries = implode(', ',$count);	
+    unset($row);
+	unset($itemcount);
+	unset($count);
  $html=<<<cd
  
  <script src="../lib/highcharts/js/highcharts.js"></script>
@@ -35,7 +49,7 @@
  $(function () {
         $('#pnlnews').highcharts({
            chart: {		
-				 type: 'spline',		   
+				 type: 'area',		   
 				width: 800,
 				height:600,
 				zoomType: 'xy'
@@ -43,25 +57,69 @@
             title: {
                 text: 'نمودار اخبار'
             },
-            xAxis: {			 
-                categories: [{$xAxis}]
+            xAxis: {
+				style: {
+                    font: 'bold 15px bmira,tahoma'
+                },			
+                categories: [{$xnAxis}]
+            },
+			yAxis: {
+                title: {
+                    text: 'تعداد خبر '
+                }
             },
             credits: {
                 enabled: false
             },
+			
             series: [{
 			    title: {
-                    text: 'تعداد خبر'
+                    text: 'تعداد'
                 },
-                name: 'تعداد خبر',
-                data: [{$series}]
+                name: 'تعداد اخبار ',
+				color: '#8bbc21',
+                data: [{$nseries}]
                 }]
         });
     });
-		
+	$(function () {
+        $('#pnlworks').highcharts({
+           chart: {		
+				 type: 'area',		   
+				width: 800,
+				height:600,
+				zoomType: 'xy'
+			},			
+            title: {
+                text: 'نمودار فعالیت های انجام شده'
+            },
+            xAxis: {			 
+                categories: [{$xwAxis}]
+            },
+			yAxis: {
+                title: {
+                    text: 'تعداد فعالیت '
+                }
+            },
+            credits: {
+                enabled: false
+            },
+			
+            series: [{
+			    title: {
+                    text: 'تعداد'
+                },
+                name: 'تعداد فعالیت ',
+                data: [{$wseries}]
+                }]
+        });
+    });
+
 	</script>
 
- <div id="pnlnews" style="width: 400px; height: 400px; margin: 0 auto"></div>
+ <div id="pnlnews" style="width: 400px; height: 400px; margin: 0;"></div>
+ <hr/><br/>
+ <div id="pnlworks" style="width: 400px; height: 400px; margin:650px 0;"></div>
 
 cd;
  return $html;
