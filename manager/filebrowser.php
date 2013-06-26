9c47c93c5b58141707e84334e33ba7bc59466539
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-06-26 10:38:38 +0330

    add others pic

--- manager/filebrowser.php
+++ manager/filebrowser.php
@@ -18,7 +18,58 @@ $html=<<<cd
 			<a href="#" title="" class="button">انتخاب</a>
 			<a href="#" title="" class="button">خروج</a>
 		</div>
-		<div class="files"></div>
+		<div class="files right">
+			<div class="pics">
+				<ul>
+					<li>
+						<div class="pic">
+							<a title="انتخاب عکس">
+								<img src="../themes/default/images/main/others/slide1.jpg" alt="" />
+								<div class="overlay"></div>
+							</a>
+						</div>
+						<h2><span>نام فایل: </span>اسلاید یک</h2>
+					</li>
+					<li>
+						<div class="pic">
+							<a title="انتخاب عکس">
+								<img src="../themes/default/images/main/others/slide2.jpg" alt="" />
+								<div class="overlay"></div>
+							</a>
+						</div>
+						<h2><span>نام فایل: </span>اسلاید یک</h2>
+					</li>
+					<li>
+						<div class="pic">
+							<a title="انتخاب عکس">
+								<img src="../themes/default/images/main/others/slide3.jpg" alt="" />
+								<div class="overlay"></div>
+							</a>
+						</div>
+						<h2><span>نام فایل: </span>اسلاید یک</h2>
+					</li>
+					<li>
+						<div class="pic">
+							<a title="انتخاب عکس">
+								<img src="../themes/default/images/main/others/slide4.jpg" alt="" />
+								<div class="overlay"></div>
+							</a>
+						</div>
+						<h2><span>نام فایل: </span>اسلاید یک</h2>
+					</li>
+					<li>
+						<div class="pic">
+							<a title="انتخاب عکس">
+								<img src="../themes/default/images/main/others/slide1.jpg" alt="" />
+								<div class="overlay"></div>
+							</a>
+						</div>
+						<h2><span>نام فایل: </span>اسلاید یک</h2>
+					</li>
+				</ul>
+				<div class="badboy"></div>
+			</div>
+		</div>
 		<div class="badboy"></div>
 	</div>
 cd;
