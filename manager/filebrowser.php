fff9f7fc746bdc9567e704bc54eead989843e146
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-06-26 09:42:04 +0330

    add pic

--- manager/filebrowser.php
+++ manager/filebrowser.php
@@ -7,7 +7,11 @@
 	include_once("../lib/persiandate.php");		
 $html=<<<cd
 	<div class="picmanager">
-		<div class="prev right"></div>
+		<div class="prev right">
+			<div class="pic">
+				<img src="../themes/default/images/main/others/slide1.jpg" alt="">
+			</div>
+		</div>
 		<div class="files"></div>
 		<div class="badboy"></div>
 	</div>
