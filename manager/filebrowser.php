33ca5e756cdda40f6dda4c92a819650a49771f8f
Saeid Hatami <hatami4510@gmail.com>
2013-06-28 18:10:13 +0330

    add file browsing code

--- manager/filebrowser.php
+++ manager/filebrowser.php
@@ -4,7 +4,35 @@
 	include_once("../classes/messages.php");
 	include_once("../classes/session.php");	
 	include_once("../classes/functions.php");
-	include_once("../lib/persiandate.php");		
+	include_once("../lib/persiandate.php");	    
+	$handle=opendir('../newspics');
+    while ($file = readdir($handle))
+    {        
+         if (!preg_match("/^[.]/",$file,$out, PREG_OFFSET_CAPTURE))
+         {             
+			 if(is_dir("../newspics/".$file))
+			 {                              
+					  $dirname = basename($file);
+                      $pics=<<<cd
+					     <li>
+						<div class="pic">
+							<a class="select" title="انتخاب عکس">
+								<img src="{$dirname}" alt="" />
+								<div class="overlay"></div>
+							</a>
+						</div>
+						<h2><span class="highlight">نام فایل: </span><span class="filename">{$dirname}</span></h2>
+					</li>
+cd;
+					 // echo $dirname;                                    
+					  $newspics[] = array("Name"=>basename($file),
+									   "Size"=>(filesize($file)/1024),//KB
+									   "ext"=> substr($file, strrpos($file, '.') + 1));
+			  }
+        }
+    }
+   
+    closedir($handle);  
 $html=<<<cd
 	<div class="picmanager">
 		<div class="prev right">
@@ -22,51 +50,7 @@ $html=<<<cd
 		<div class="files right">
 			<div class="pics">
 				<ul>
-					<li>
-						<div class="pic">
-							<a class="select" title="انتخاب عکس">
-								<img src="../themes/default/images/main/others/slide1.jpg" alt="" />
-								<div class="overlay"></div>
-							</a>
-						</div>
-						<h2><span class="highlight">نام فایل: </span><span class="filename">اسلاید یک</span></h2>
-					</li>
-					<li>
-						<div class="pic">
-							<a title="انتخاب عکس">
-								<img src="../themes/default/images/main/others/slide2.jpg" alt="" />
-								<div class="overlay"></div>
-							</a>
-						</div>
-						<h2><span class="highlight">نام فایل: </span><span class="filename">اسلاید دو</span></h2>
-					</li>
-					<li>
-						<div class="pic">
-							<a title="انتخاب عکس">
-								<img src="../themes/default/images/main/others/slide3.jpg" alt="" />
-								<div class="overlay"></div>
-							</a>
-						</div>
-						<h2><span class="highlight">نام فایل: </span><span class="filename">اسلاید سه</span></h2>
-					</li>
-					<li>
-						<div class="pic">
-							<a title="انتخاب عکس">
-								<img src="../themes/default/images/main/others/slide4.jpg" alt="" />
-								<div class="overlay"></div>
-							</a>
-						</div>
-						<h2><span class="highlight">نام فایل: </span><span class="filename">اسلاید چهار</span></h2>
-					</li>
-					<li>
-						<div class="pic">
-							<a title="انتخاب عکس">
-								<img src="../themes/default/images/main/others/logo.png" alt="" />
-								<div class="overlay"></div>
-							</a>
-						</div>
-						<h2><span class="highlight">نام فایل: </span><span class="filename">لگو</span></h2>
-					</li>
+					{$pics}
 				</ul>
 				<div class="badboy"></div>
 			</div>
