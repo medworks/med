8ea7d8977791706ada7038d0ab3730c8b20fc289
Saeid Hatami <hatami4510@gmail.com>
2013-07-04 20:32:16 +0330

    add logo of project

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/***************************************************************************
+ *                              MediaTeq CMS                               *
+ *                          -----------------------                        *
+ *   Copyright            : (C) 2013 The MediaTeq Team                     *
+ *   Email                : info@mediaTeq.ir                               *
+ *   Programmers          : Saeid Hatami  &  Mojtaba Amjadi                *
+ *   Start Date           : 1392-02-20 ,Friday,2013-05-10                  * 
+ ***************************************************************************/
 	error_reporting (E_ALL ^ E_NOTICE);
     include_once("./classes/functions.php");
 	include_once("./themes/default/main.php");
