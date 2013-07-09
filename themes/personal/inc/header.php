17645dc3fc4acfab30fec174f404799179c313c8
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-07-09 17:46:21 +0330

    strat to add new style

---
+++ themes/personal/inc/header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="chrome=1">
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="themes/css/1styles.css" />
+	<link rel="stylesheet" href="themes/css/prettyphoto.css" />
+	<link rel="stylesheet" href="themes/css/validationEngine.css"/>
+	<link rel="stylesheet" href="themes/personal/style.css" />
+
+	<script src="lib/js/jquery.js" type="text/javascript"></script>
+	<script src="lib/js/jquery.cycle.all.js" type="text/javascript"></script>
+	<script src="lib/js/jquery.validationEngine-en.js" type="text/javascript"></script>
+	<script src="lib/js/jquery.validationEngine.js" type="text/javascript"></script>	
+	<script src="lib/js/CFInstall.js" type="text/javascript"></script>
+	<script src="lib/js/jquery.validationEngine-en.js" type="text/javascript"></script>
+	<script src="lib/js/jquery.validationEngine.js" type="text/javascript"></script>
+	<script src="themes/js/jquery.prettyphoto.js" type="text/javascript"></script>	
+	<script src="themes/personal/js/script.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="lib/js/html5shiv.js"></script>
+	<![endif]-->
+	<link rel="Shortcut Icon" href="themes/personal/favicon.ico" />
+</head>
+<body>
+	
+</body>
+</html>
