7d39c333b67987928fe8fc128eb5f8be2bd13dd7
Saeid Hatami <Hatami4510@gmail.com>
2013-07-10 20:04:06 +0430

    add change theme ability

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
  *   Programmers          : Saeid Hatami  &  Mojtaba Amjadi                *
  *   Start Date           : 1392-02-20 ,Friday,2013-05-10                  * 
  ***************************************************************************/
+    include_once("config.php");
+    include_once("classes/database.php");
+	include_once("./classes/functions.php");
 	error_reporting (E_ALL ^ E_NOTICE);
-    include_once("./classes/functions.php");
-	include_once("./themes/default/main.php");
+	$db = Database::GetDatabase();
+	$theme = GetSettingValue('Site_Theme_Name',0);    
+	include_once("./themes/{$theme}/main.php");
 	//include_once("./themes/personal/main.php");	
 ?>
