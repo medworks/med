1cad854ffc6779f391311170556eeb45780bda35
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-07-10 13:15:27 +0330

    add slideshow

--- themes/personal/inc/header.php
+++ themes/personal/inc/header.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="themes/css/1styles.css" />
 	<link rel="stylesheet" href="themes/css/prettyphoto.css" />
 	<link rel="stylesheet" href="themes/css/validationEngine.css"/>
+	<link rel="stylesheet" href="themes/personal/settings.css" />
 	<link rel="stylesheet" href="themes/personal/style.css" />
 
 	<script src="lib/js/jquery.js" type="text/javascript"></script>
@@ -17,6 +18,8 @@
 	<script src="lib/js/jquery.validationEngine-en.js" type="text/javascript"></script>
 	<script src="lib/js/jquery.validationEngine.js" type="text/javascript"></script>
 	<script src="themes/js/jquery.prettyphoto.js" type="text/javascript"></script>	
+	<script src="themes/personal/js/jquery.themepunch.plugins.min.js" type="text/javascript"></script>
+	<script src="themes/personal/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script>
 	<script src="themes/personal/js/script.js" type="text/javascript"></script>
 	<!--[if lt IE 9]>
 		<script src="lib/js/html5shiv.js"></script>
@@ -52,7 +55,146 @@
 			<div class="contain">
 				<div class="inner">
 					<div class="inner-wrap">
-						<div class="slideshow"></div>
+						<div class="slideshow ltr bannercontainer responsive">
+							<div class="banner ltr">
+								<ul>
+						            <!-- THE 1 SLIDE -->
+						            <li data-transition="turnoff" data-slotamount="1" data-masterspeed="300" >
+						                        <img src="themes/personal/images/others/slide1.png" >
+
+						                        <div class="caption large_text fade"
+						                             data-x="509"
+						                             data-y="290"
+						                             data-speed="300"
+						                             data-start="800"
+						                             data-easing="easeOutExpo"  ></div>
+
+						                        <div class="caption very_large_black_text randomrotate"
+						                             data-x="641"
+						                             data-y="95"
+						                             data-speed="300"
+						                             data-start="1100"
+						                             data-easing="easeOutExpo"  >SLIDER</div>
+
+						                        <div class="caption large_black_text randomrotate"
+						                             data-x="642"
+						                             data-y="161"
+						                             data-speed="300"
+						                             data-start="1400"
+						                             data-easing="easeOutExpo"  >REVOLUTION</div>
+
+						                        <div class="caption bold_red_text randomrotate"
+						                             data-x="645"
+						                             data-y="201"
+						                             data-speed="300"
+						                             data-start="1700"
+						                             data-easing="easeOutExpo"  >GOT FULLSCREEN VIDEO</div>
+
+						                        <div class="caption sfb"
+						                             data-x="640"
+						                             data-y="223"
+						                             data-speed="300"
+						                             data-start="2000"
+						                             data-easing="easeOutBack"  >
+						                             <!-- <img src="images/slides/video.jpg" alt="Image 7"> --> 
+						                         </div>
+
+						                        <div class="caption sft"
+						                             data-x="639"
+						                             data-y="383"
+						                             data-speed="300"
+						                             data-start="2300"
+						                             data-easing="easeOutExpo"  data-linktoslide="4" >
+						                            <!-- <img src="images/slides/videobutton.jpg" alt="Image 8"> -->
+						                         </div>
+						            </li>
+
+						            <!-- THE 2 SLIDE -->
+						            <li data-transition="slideleft" data-slotamount="1" data-masterspeed="300" data-thumb="images/thumbs/thumb5.jpg">
+
+						                    <img src="themes/personal/images/others/slide2.jpg" >
+
+						                    <div class="caption large_text sft"
+						                         data-x="50"
+						                         data-y="44"
+						                         data-speed="300"
+						                         data-start="800"
+						                         data-easing="easeOutExpo"  >TOUCH ENABLED</div>
+
+						                    <div class="caption medium_text sfl"
+						                         data-x="79"
+						                         data-y="82"
+						                         data-speed="300"
+						                         data-start="1100"
+						                         data-easing="easeOutExpo"  >AND</div>
+
+						                    <div class="caption large_text sfr"
+						                         data-x="128"
+						                         data-y="78"
+						                         data-speed="300"
+						                         data-start="1100"
+						                         data-easing="easeOutExpo"  ><span style="color: #ffc600;">RESPONSIVE</span></div>
+
+						                    <div class="caption lfl"
+						                         data-x="53"
+						                         data-y="192"
+						                         data-speed="300"
+						                         data-start="1400"
+						                         data-easing="easeOutExpo"  >
+						                         <!-- <img src="images/slides/imac.png" alt="Image 4"> -->
+						                     </div>
+
+						                    <div class="caption lfl"
+						                         data-x="253"
+						                         data-y="282"
+						                         data-speed="300"
+						                         data-start="1500"
+						                         data-easing="easeOutExpo"  >
+						                         <!-- <img src="images/slides/ipad.png" alt="Image 5"> -->
+						                     </div>
+
+						                    <div class="caption lfl"
+						                         data-x="322"
+						                         data-y="313"
+						                         data-speed="300"
+						                         data-start="1600"
+						                         data-easing="easeOutExpo"  >
+						                         <!-- <img src="images/slides/iphone.png" alt="Image 6"> -->
+						                     </div>
+						            </li>
+
+
+						            <!-- THE 3 SLIDE -->
+						            <li data-transition="flyin" data-slotamount="1" data-masterspeed="300" data-thumb="images/thumbs/thumb6.jpg">
+						                    <img src="themes/personal/images/others/slide3.jpg" >
+
+						                    <div class="caption large_text sfl"
+						                         data-x="38"
+						                         data-y="200"
+						                         data-speed="300"
+						                         data-start="1000"
+						                         data-easing="easeOutExpo"  >A Happy</div>
+
+						                    <div class="caption large_text sfl"
+						                         data-x="37"
+						                         data-y="243"
+						                         data-speed="300"
+						                         data-start="1300"
+						                         data-easing="easeOutExpo"  >Holiday Season</div>
+
+						                    <div class="caption randomrotate"
+						                         data-x="610"
+						                         data-y="174"
+						                         data-speed="800"
+						                         data-start="1600"
+						                         data-easing="easeOutExpo"  >
+						                         <!-- <img src="images/slides/TP_logo.png" alt="Image 4"> -->
+						                     </div>
+						            </li>
+						        </ul>
+						        <div class="tp-bannertimer"></div>
+							</div>
+						</div>
 						<div class="client-part"></div>
 						<div class="works-part"></div>
 						<div class="tabs-part"></div>
@@ -63,6 +205,38 @@
 			</div>
 			<div class="badboy"></div>
 		</section>
+		<script>
+		    jQuery(document).ready(function() {
+		       jQuery('.banner').revolution(
+		          {
+		              delay:5000,
+		              startheight:500,
+		              startwidth:960,
+		    
+		              hideThumbs:300,
+		    
+		              thumbWidth:100,	
+		              thumbHeight:50,
+		              thumbAmount:5,
+		    
+		              navigationType:"both",					
+		              navigationArrows:"verticalcentered",		
+		              navigationStyle:"round",				
+		    
+		              touchenabled:"on",					
+		              onHoverStop:"on",					
+		    
+		              navOffsetHorizontal:0,
+		              navOffsetVertical:20,
+		    
+		              stopAtSlide:-1,
+		              stopAfterLoops:-1,
+		    
+		              shadow:1,						
+		              fullWidth:"off"					
+		          });
+		    });
+		</script>
 		<!--  -->
 	</div>
 </body>
