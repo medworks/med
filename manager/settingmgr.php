ce3a55f1604bb7e4802fca3e8a1be5bf83186190
Saeid Hatami <Hatami4510@gmail.com>
2013-07-11 18:23:33 +0430

    add options to settingmgr page

--- manager/settingmgr.php
+++ manager/settingmgr.php
@@ -13,6 +13,43 @@
 		die(); // solve a security bug
 	 }
 	$db = Database::GetDatabase();
+	if ($_GET['act']=="do")
+{
+	$html=<<<ht
+		<div class="title">
+	      <ul>
+	        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
+	        <li><span>مدیریت اخبار</span></li>
+	      </ul>
+	      <div class="badboy"></div>
+	    </div>
+		<div class="sub-menu" id="mainnav">
+			<ul>
+			  <li>		  
+				<a href="?item=settingmgr&act=about">متن درباره ما
+					<span class="add-user"></span>
+				</a>
+			  </li>
+			  <li>
+				<a href="?item=settingmgr&act=seo" >اطلاعات سئو 
+					<span class="edit-user"></span>
+				</a>
+			  </li>
+			  <li>
+				<a href="?item=settingmgr&act=emails" >ایمیل ها 
+					<span class="edit-user"></span>
+				</a>
+			  </li>
+			  <li>
+				<a href="?item=settingmgr&act=grid" >جداول اطلاعات 
+					<span class="edit-user"></span>
+				</a>
+			  </li>
+			 </ul>
+			 <div class="badboy"></div>
+		</div>		 
+ht;
+}
 	
 return $html;
 ?>
