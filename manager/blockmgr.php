8cd1cfc493e38df26c8e2363619223640b80861b
Saeid Hatami <Hatami4510@gmail.com>
2013-07-15 22:53:46 +0430

    add codes for blockmgr page

--- manager/blockmgr.php
+++ manager/blockmgr.php
@@ -14,6 +14,32 @@
 	if ($_GET['item']!="blockmgr")	exit();
 	$db = Database::GetDatabase();
 	$msg = Message::GetMessage();
-	
+	if ($_GET['act']=="do")
+{
+	$html=<<<ht
+		<div class="title">
+	      <ul>
+	        <li><a href="?item=dashboard&act=do">پیشخوان</a></li>
+	        <li><span>مدیریت بلاک ها</span></li>
+	      </ul>
+	      <div class="badboy"></div>
+	    </div>
+		<div class="sub-menu" id="mainnav">
+			<ul>
+			  <li>		  
+				<a href="?item=blockmgr&act=new"> ایجاد بلاک
+					<span class="add-works"></span>
+				</a>
+			  </li>
+			  <li>
+				<a href="?item=blockmgr&act=mgr" >حذف/ویرایش بلاک
+					<span class="edit-works"></span>
+				</a>
+			  </li>
+			 </ul>
+			 <div class="badboy"></div>
+		</div>		 
+ht;
+}
 return $html;
 ?>
