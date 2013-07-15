8b48850781c06a92ddcf363f874075a833376098
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-07-15 11:08:20 +0330

    add sub menu

--- themes/personal/inc/menu.php
+++ themes/personal/inc/menu.php
@@ -1,7 +1,14 @@
 <section class="middle">
 	<div class="main-menu">
 		<menu>
-			<li><a href="./"><span class="home-icon"></span>خانه</a></li>
+			<li><a href="./"><span class="home-icon"></span>خانه</a>
+				<menu>
+					<li><a href="#">خانه 1</a></li>
+					<li><a href="#">خانه 2</a></li>
+					<li><a href="#">خانه 3</a></li>
+					<li><a href="#">خانه 4</a></li>
+				</menu>
+			</li>
 			<li><a href="#"><span class="contact-icon"></span>تماس با ما</a></li>
 		</menu>
 	</div>
