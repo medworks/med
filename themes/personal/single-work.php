be6a015eb88a388f96f483e0820bac70f2263d3d
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-07-20 17:43:34 +0330

    link database to single work

--- themes/personal/single-work.php
+++ themes/personal/single-work.php
@@ -1,19 +1,22 @@
 <?php
+	include_once("./classes/database.php");
+	$db = database::getDatabase();
+	$works = $db->Select('works',NULL,"id={$_GET[wid]}"," sdate DESC");
 $html=<<<cd
 	<div class="singlework-page" id="others-page">
 		<div class="page-header">
-			<h1><span>کارهای ما / </span>تیتر کار اول</h1>
+			<h1><span>کارهای ما / </span>{$works[subject]}</h1>
 			<div class="badboy"></div>
 		</div>
 		<div class="badboy"></div>
 		<div class="singlework" id="special-page">
 			<div class="pic">
-				<img src="themes/personal/images/others/slide2.jpg" alt="" />
+				<img src="{$works[image]}" alt="{$works[subject]}" />
 			</div>
 			<div class='detail'>
-				<h2>تیتر کار اول</h2>
-				<p class="type">گرافیکی</p>
-				<p class="text">توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... </p>
+				<h2>{$works[subject]}</h2>
+				<!-- <p class="type">گرافیکی</p> -->
+				{$works[body]}
 			</div>
 		</div>
 	</div>
