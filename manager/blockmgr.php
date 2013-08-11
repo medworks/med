a653cbeb32d0eef5fe7db5f5fc2aff6d5409e3f3
Saeid Hatami <hatami4510@gmail.com>
2013-08-11 12:56:53 +0330

    complete blockmgr page

--- manager/blockmgr.php
+++ manager/blockmgr.php
@@ -15,7 +15,7 @@
 	$db = Database::GetDatabase();
 	$msg = Message::GetMessage();
 	if ($_GET['act']=="do")
-{
+	{
 	$html=<<<ht
 		<div class="title">
 	      <ul>
@@ -40,6 +40,29 @@
 			 <div class="badboy"></div>
 		</div>		 
 ht;
+} else
+if ($_GET['act']=="new" or $_GET['act']=="edit")
+{
+$msgs = GetMessage($_GET['msg']);	
+$html=<<<cd
+	<script type='text/javascript'>
+		$(document).ready(function(){	   
+			$("#frmnewsmgr").validationEngine();			
+    });
+	</script>	   
+  <div class="title">
+      <ul>
+        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
+	    <li><span>مدیریت اخبار</span></li>
+      </ul>
+      <div class="badboy"></div>
+  </div>
+  <div class="mes" id="message">{$msgs}</div>
+  <div class='content'>
+	<form name="frmblocks" id="frmblocks" class="" action="" method="post" >
+     <p class="note">پر کردن موارد مشخص شده با * الزامی می باشد</p>
+	</form> 
+cd;
 }
 return $html;
 ?>
