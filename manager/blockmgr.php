fd064f6b486d9f5131ea69ba3b75f35584434481
Saeid Hatami <hatami4510@gmail.com>
2013-08-12 19:18:29 +0330

    fix errors and complete coding

--- manager/blockmgr.php
+++ manager/blockmgr.php
@@ -11,7 +11,7 @@
 		header("Location: ../index.php");
 		die(); // solve a security bug
 	}
-	if ($_GET['item']!="blockmgr")	exit();
+	if ($_GET['item']!="blocksmgr")	exit();
 	$db = Database::GetDatabase();
 	$msg = Message::GetMessage();
 	if ($_GET['act']=="do")
@@ -27,12 +27,12 @@
 		<div class="sub-menu" id="mainnav">
 			<ul>
 			  <li>		  
-				<a href="?item=blockmgr&act=new"> ایجاد بلاک
+				<a href="?item=blocksmgr&act=new"> ایجاد بلاک
 					<span class="add-block"></span>
 				</a>
 			  </li>
 			  <li>
-				<a href="?item=blockmgr&act=mgr" >حذف/ویرایش بلاک
+				<a href="?item=blocksmgr&act=mgr" >حذف/ویرایش بلاک
 					<span class="edit-block"></span>
 				</a>
 			  </li>
@@ -109,6 +109,9 @@ $html=<<<cd
          <span>*</span>
       </p>   
 	  <textarea cols="50" rows="10" name="content" class="detail" id="content" > </textarea>
+	  {$editorinsert}       
+      	 <input type="reset" value="پاک کردن" class='reset' /> 	 	     
+       </p>  
 	</form> 
 
 <!-- TinyMCE -->
@@ -172,6 +175,118 @@ $html=<<<cd
 	<!-- /TinyMCE -->   	
 	
 cd;
-}
+}else
+if ($_GET['act']=="mgr")
+{
+	if ($_POST["mark"]=="srhnews")
+	{	 		
+	    
+		$rows = $db->SelectAll(
+				"block",
+				"*",
+				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",				
+				$_GET["pageNo"]*10,
+				10);
+			if (!$rows) 
+			{					
+				$_GET['item'] = "blocksmgr";
+				$_GET['act'] = "mgr";
+				$_GET['msg'] = 6;				
+				//header("Location:?item=newsmgr&act=mgr&msg=6");
+			}
+		
+	}
+	else
+	{	
+		$rows = $db->SelectAll(
+				"block",
+				"*",
+				null,				
+				$_GET["pageNo"]*10,
+				10);
+    }
+                $rowsClass = array();
+                $colsClass = array();
+                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhnews")?$db->CountAll("block"):Count($rows);
+                for($i = 0; $i < Count($rows); $i++)
+                {						
+		        $rows[$i]["name"] =(mb_strlen($rows[$i]["name"])>20)?mb_substr($rows[$i]["name"],0,20,"UTF-8")."...":$rows[$i]["name"];                
+				if ($i % 2==0)
+				 {
+						$rowsClass[] = "datagridevenrow";
+				 }
+				else
+				{
+						$rowsClass[] = "datagridoddrow";
+				}				
+				$rows[$i]["edit"] = "<a href='?item=blocksmgr&act=edit&bid={$rows[$i]["id"]}' class='edit-field'" .
+						"style='text-decoration:none;'></a>";								
+				$rows[$i]["delete"]=<<< del
+				<a href="javascript:void(0)"
+				onclick="DelMsg('{$rows[$i]['id']}',
+					'از حذف این بلاک اطمینان دارید؟',
+				'?item=blocksmgr&act=del&pageNo={$_GET[pageNo]}&bid=');"
+				 class='del-field' style='text-decoration:none;'></a>
+del;
+                         }
+
+    if (!$_GET["pageNo"] or $_GET["pageNo"]<=0) $_GET["pageNo"] = 0;
+            if (Count($rows) > 0)
+            {                    
+                    $gridcode .= DataGrid(array( 
+							"name"=>"نام",
+							"pos"=>"موقعیت مکانی",
+							"order"=>"ترتیب مکانی",
+							"acclevel"=>"سطح دسترسی",
+							"plugin"=>"پلاگین",							
+							"contenttype"=>"نوع محتوا",
+                            "edit"=>"ویرایش",
+							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=blocksmgr&act=mgr");
+                    
+            }
+$msgs = GetMessage($_GET['msg']);
+$list = array("name"=>"عنوان");
+$combobox = SelectOptionTag("cbsearch",$list,"name");
+$code=<<<edit
+<script type='text/javascript'>
+	$(document).ready(function(){	   			
+		$('#srhsubmit').click(function(){	
+			$('#frmsrh').submit();
+			return false;
+		});		
+	});
+</script>	   
+					<div class="title">
+				      <ul>
+				        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
+					    <li><span>مدیریت بلاک ها</span></li>
+				      </ul>
+				      <div class="badboy"></div>
+				  </div>
+                    <div class="Top">                       
+						<center>
+							<form action="?item=blocksmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+								<p>جستجو بر اساس {$combobox}</p>
+
+								<p class="search-form">
+									<input type="text" id="date_input_1" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  /> 
+									<img src="../themes/default/images/admin/cal.png" class="cal-btn" id="date_btn_2" alt="cal-pic">
+							        
+									<a href="?item=blocksmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=blocksmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+								</p>
+								<input type="hidden" name="mark" value="srhnews" /> 
+								{$msgs}
+
+								{$gridcode} 
+															
+							</form>
+					   </center>
+					</div>
+
+edit;
+$html = $code;
+}	
 return $html;
 ?>
