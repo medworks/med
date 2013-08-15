1990b7e2be6047528580df006a94b63e39ce6301
Saeid Hatami <hatami4510@gmail.com>
2013-08-15 14:22:58 +0330

    add new page for ajax commands

---
+++ manager/ajaxcommand.php
@@ -0,0 +1,22 @@
+<?php
+	include_once("../config.php");
+    include_once("../classes/database.php");
+	include_once("../classes/messages.php");
+	include_once("../classes/session.php");	
+	include_once("../classes/functions.php");
+	include_once("../classes/login.php");
+	include_once("../lib/persiandate.php");	
+	$login = Login::getLogin();
+	if (!$login->IsLogged())
+	{
+		header("Location: ../index.php");
+		die(); // solve a security bug
+	}
+	$db = Database::GetDatabase();
+ if (isset($_GET["sec"]))
+{
+	$category = $db->SelectAll("category","*","secid={$_GET[sec]}","id ASC");
+	$cbcategory = DbSelectOptionTag("cbcat",$category,"catname");
+	echo $cbcategory;
+}
+?>
