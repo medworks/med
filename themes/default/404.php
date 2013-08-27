145ff63636ed45276411eef519fb7fbd95e8d6a4
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-08-27 11:27:01 +0330

    add 404 page

---
+++ themes/default/404.php
@@ -0,0 +1,19 @@
+<?php
+
+$html=<<<cd
+	<div class="content">
+		<div class='not-found'>
+			<div class="pic404">
+				<img src="themes/default/images/404.png" alt="404" />
+			</div>
+			<div class="text404">
+				<p>صفحه مورد نظر یافت نشد</p>
+				<p>Page not found</p>
+			</div>
+		</div>
+	</div>	
+cd;
+
+return $html;
+
+?>
