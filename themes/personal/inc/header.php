14ddad95d122011452bd5f0ea44dbd5debbb8b68
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-02 18:05:27 +0330

    add analytics

--- themes/personal/inc/header.php
+++ themes/personal/inc/header.php
@@ -33,12 +33,25 @@
 	<!--[if lt IE 9]>
 		<script src="lib/js/html5shiv.js"></script>
 	<![endif]-->
-	<script type="text/javascript" src="http://counter.toolsir.com/secure.js"></script>
+	<!-- Start Open Web Analytics Tracker -->
 	<script type="text/javascript">
-		var _torder = '&pic=1';
-		var _turl = 'http://www.mediateq.ir';
-		_tCounter (_torder,_turl);
+		//<![CDATA[
+		var owa_baseUrl = 'http://mediateq.ir/owa/';
+		var owa_cmds = owa_cmds || [];
+		owa_cmds.push(['setSiteId', 'b7f6a5c325ee3b6052784404b8d2671d']);
+		owa_cmds.push(['trackPageView']);
+		owa_cmds.push(['trackClicks']);
+		owa_cmds.push(['trackDomStream']);
+
+		(function() {
+			var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+			owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+			_owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
+			var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+		}());
+		//]]>
 	</script>
+	<!-- End Open Web Analytics Code -->
 	<link rel="Shortcut Icon" href="themes/personal/favicon.ico" />
 </head>
 <body>
