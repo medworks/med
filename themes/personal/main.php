205e6a86ef34375c011ef046afd2a149b7c502f5
Saeid Hatami <hatami4510@gmail.com>
2013-09-07 20:01:04 +0330

    add title changing to this theme

--- themes/personal/main.php
+++ themes/personal/main.php
@@ -1,7 +1,9 @@
 <?php
-	include_once("inc/header.php");
-	
+	ob_start();
 	include_once("inc/menu.php");
-
+	$page_content = ob_get_contents();
+	ob_end_clean();	
+	include_once("inc/header.php");
+	echo $page_content;	
 	include_once("inc/footer.php");
 ?>
