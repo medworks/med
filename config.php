56e89b1c69984a50c087b960443aa083a9c0908f
Saeid Hatami <Hatami4510@gmail.com>
2013-09-08 22:39:57 +0430

    just for testing new version of smartgit

--- config.php
+++ config.php
@@ -19,7 +19,7 @@
   define("_USER_IP",$_SERVER['REMOTE_ADDR']);  
   define("_ADMIN", 1);
   define("_USER", 2);
-  define("_ANONYMOUS", 3);
+  define("_ANONYMOUS", 3); 
 /*
   define ('SITE_ROOT', '');
   define ('OS_ROOT', $_SERVER['DOCUMENT_ROOT']);
