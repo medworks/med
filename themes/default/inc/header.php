b1f94d6ca4a374c88319780b641f28f51878fa64
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-09-09 11:56:57 +0330

    fix meta tag

--- themes/default/inc/header.php
+++ themes/default/inc/header.php
@@ -9,11 +9,15 @@
 <!doctype html>
 <html lang="fa">
 <head>
+	<title><?php echo $seo->Site_Title;?></title>
+
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=IE8">
 	<meta charset="UTF-8">
-	<title><?php echo $seo->Site_Title;?></title>
-	<meta name="description" content="<?php echo $seo->Site_Describtion;?> "/>
-	<meta name="keywords" content="<?php echo $seo->Site_KeyWords;?> "/>
+	<meta name="description" content="<?php echo $seo->Site_Describtion;?>"/>
+	<meta name="keywords" content="<?php echo $seo->Site_KeyWords;?>"/>
+	<meta name="robots" content="INDEX,FOLLOW">
+	<meta http-equiv="Content-Language"  content="Fa">
+
 	<link rel="stylesheet" href="themes/css/1styles.css" />
 	<link rel="stylesheet" href="themes/css/prettyphoto.css" />
 	<link rel="stylesheet" href="themes/css/validationEngine.css"/>
@@ -48,7 +52,8 @@
 	<!--[if lt IE 9]>
 		<script src="lib/js/html5shiv.js"></script>
 	<![endif]-->
-	<link rel="Shortcut Icon" href="themes/default/favicon.ico" />
+	<link rel="Shortcut Icon" href="themes/default/favicon.ico" type="image/x-icon"/>
+	<link rel="icon" href="themes/personal/favicon.ico" type="image/x-icon"/>
 </head>
 <body>
 	  <!--[if IE]>
