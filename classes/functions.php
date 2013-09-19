a4426d1302486c8984d056f9a04b61933a722c36
Saeid Hatami <hatami4510@gmail.com>
2013-09-19 12:04:32 +0330

    fix bcc errors

--- classes/functions.php
+++ classes/functions.php
@@ -244,7 +244,8 @@ function DataGrid($cols, $rows, $colsClass, $rowsClass, $itemsInPage, $pageNo, $
 		$senderName = "=?UTF-8?B?" . base64_encode($senderName) . "?=";
 		$mail->SetFrom($senderEmail, $senderName);
 		foreach($receivers as $key=>$r)
-			$mail->AddAddress($r);
+			//$mail->AddAddress($r);
+			$mail->AddBCC($r);
 		$mail->Subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
 		//$mail->MsgHTML("=?UTF-8?B?" . base64_encode($message). "?=");
                 $mail->CharSet = "utf-8";
@@ -271,7 +272,7 @@ function DataGrid($cols, $rows, $colsClass, $rowsClass, $itemsInPage, $pageNo, $
         $mail->CharSet = "utf-8";
         $mail->MsgHTML($message);
         $mail->WordWrap = 50;
-        foreach($receivers as $key=>$r) $mail->AddAddress($r);
+        foreach($receivers as $key=>$r) $mail->AddBCC($r);//$mail->AddAddress($r);
         $mail->IsHTML(true);
         return $mail->Send();
       }
