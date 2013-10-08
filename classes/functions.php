1ae94cfdfed078d4324154d9599f18d8e931f6a6
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-10-08 19:40:45 +0330

    fix ajax for contact form

--- classes/functions.php
+++ classes/functions.php
@@ -19,7 +19,7 @@
 			break;
 			case 'contact':			    			
 				$seo->Site_Title = "تماس با ما";
-                return "themes/default/contact.html";
+                return "themes/default/contact.php";
 			break;
 			case 'gallery':
 			    $seo->Site_Title = "گالری تصاویر";
--- manager/ajaxcommand.php
+++ manager/ajaxcommand.php
@@ -25,6 +25,38 @@ if ($_GET["news"]=="reg")
 	}
 		 
 }
+
+if($_GET["contact"]=="reg"){
+	$msg="";
+	$admin = 'info@mediateq.ir';
+
+	$name    = $_POST['family'];
+	$email   = $_POST['email'];
+	$subject = $_POST['subject'];
+	$text    = $_POST['message'];
+
+	$message = "$text";
+
+	if( strlen($name)>=1 && checkEmail($email) && strlen($text)>=1 ){
+		if( @mail (
+				$admin,
+				"$subject",
+				$message,
+				"From:$name $email" )
+		){
+			$msg="OK";
+
+		}else{
+			$msg="<div class='notification_error rtl'>خطا! پیام شما ارسال نشد لطفا مجددا تلاش نمایید.</div>";
+
+		}
+	}else{
+		$msg="<div class='notification_error rtl'>خطا! لطفا فیلدها را بررسی نمایید و مجددا ارسال کنید!</div>";
+	}
+
+echo $msg;
+}
+
  if (isset($_GET["sec"]))
 {
 	$category = $db->SelectAll("category","*","secid={$_GET[sec]}","id ASC");
