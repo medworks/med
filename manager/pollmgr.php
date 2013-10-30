798dbf1ef741474713f7f6e469f523060ea895a1
Saeid Hatami <hatami4510@gmail.com>
2013-10-30 20:14:01 +0330

    completting codes

--- manager/pollmgr.php
+++ manager/pollmgr.php
@@ -13,4 +13,36 @@
 	}
 	$db = Database::GetDatabase();	
 	$overall_error = false;
+	if ($_GET['item']!="pollmgr")	exit();
+	if ($_GET['act']=="do")
+	{
+		$html=<<<ht
+			<div class="title">
+			  <ul>
+				<li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
+				<li><span>مدیریت نظر سنجی</span></li>
+			  </ul>
+			  <div class="badboy"></div>
+			</div>
+			<div class="sub-menu" id="mainnav">
+				<ul>
+				  <li>		  
+					<a href="?item=pollmgr&act=new">درج نظرسنجی جدید
+						<span class="add-news"></span>
+					</a>
+				  </li>
+				  <li>
+					<a href="?item=pollmgr&act=mgr" id="poll" name="poll">حذف/ویرایش نظرسنجی
+						<span class="edit-news"></span>
+					</a>
+				  </li>
+				 </ul>
+				 <div class="badboy"></div>
+			</div>		 
+ht;
+	}
+	else
+	if ($_GET['act']=="new" or $_GET['act']=="edit")
+	{
+	}
 ?>	
