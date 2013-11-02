4e7ecfab801ae2285030b0546363830a41f6f8ec
Saeid Hatami <hatami4510@gmail.com>
2013-11-02 20:35:49 +0330

    add search & edit section codes

--- manager/pollmgr.php
+++ manager/pollmgr.php
@@ -35,6 +35,7 @@
 		   $fields = array("`pid`","`option`");				   
 		   foreach($option as $row)
 		   {
+		     $row = strip_tags($row);
 		     $values = array("'{$recid}'","'{$row}'");
 			 $db->InsertQuery('polloptions',$fields,$values);
 		   }
@@ -134,5 +135,146 @@ ht;
 	 </form>	 
 cd;
 	}
+ else
+if ($_GET['act']=="mgr")
+{
+	if ($_POST["mark"]=="srhnews")
+	{	 		
+	    if ($_POST["cbsearch"]=="ndate")
+		{
+		   date_default_timezone_set('Asia/Tehran');		   
+		   list($year,$month,$day) = explode("/", trim($_POST["txtsrh"]));		
+		   list($gyear,$gmonth,$gday) = jalali_to_gregorian($year,$month,$day);		
+		   $_POST["txtsrh"] = Date("Y-m-d",mktime(0, 0, 0, $gmonth, $gday, $gyear));
+		}
+		$rows = $db->SelectAll(
+				"polls",
+				"*",
+				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
+				"regdate DESC",
+				$_GET["pageNo"]*10,
+				10);
+			if (!$rows) 
+			{					
+				//$_GET['item'] = "pollmgr";
+				//$_GET['act'] = "mgr";
+				//$_GET['msg'] = 6;				
+				header("Location:?item=pollmgr&act=mgr&msg=6");
+			}
+		
+	}
+	else
+	{	
+		$rows = $db->SelectAll(
+				"polls",
+				"*",
+				null,
+				"regdate DESC",
+				$_GET["pageNo"]*10,
+				10);
+    }
+                $rowsClass = array();
+                $colsClass = array();
+                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhnews")?$db->CountAll("polls"):Count($rows);
+                for($i = 0; $i < Count($rows); $i++)
+                {						
+		        $rows[$i]["title"] =(mb_strlen($rows[$i]["title"])>40)?mb_substr($rows[$i]["title"],0,40,"UTF-8")."...":$rows[$i]["title"];
+                
+                $rows[$i]["regdate"] =ToJalali($rows[$i]["regdate"]," l d F  Y ");
+				                                            
+				if ($i % 2==0)
+				 {
+						$rowsClass[] = "datagridevenrow";
+				 }
+				else
+				{
+						$rowsClass[] = "datagridoddrow";
+				}
+				
+				$rows[$i]["edit"] = "<a href='?item=pollmgr&act=edit&pid={$rows[$i]["id"]}' class='edit-field'" .
+						"style='text-decoration:none;'></a>";								
+				$rows[$i]["delete"]=<<< del
+				<a href="javascript:void(0)"
+				onclick="DelMsg('{$rows[$i]['id']}',
+					'از حذف این خبر اطمینان دارید؟',
+				'?item=pollmgr&act=del&pageNo={$_GET[pageNo]}&pid=');"
+				 class='del-field' style='text-decoration:none;'></a>
+del;
+                         }
+
+    if (!$_GET["pageNo"] or $_GET["pageNo"]<=0) $_GET["pageNo"] = 0;
+            if (Count($rows) > 0)
+            {                    
+                    $gridcode .= DataGrid(array( 	
+							"title"=>"عنوان",
+							"title"=>"تصویر",
+							"regdate"=>"توضیحات",
+							"active"=>"تاریخ",
+                            "edit"=>"ویرایش",
+							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=pollmgr&act=mgr");                    
+            }
+$msgs = GetMessage($_GET['msg']);
+$list = array("title"=>"عنوان",
+              "regdate"=>"توضیحات");
+$combobox = SelectOptionTag("cbsearch",$list,"title");
+$code=<<<edit
+<script type='text/javascript'>
+	$(document).ready(function(){	   			
+		$('#srhsubmit').click(function(){	
+			$('#frmsrh').submit();
+			return false;
+		});
+		$('#cbsearch').change(function(){
+			$("select option:selected").each(function(){
+	            if($(this).val()=="regdate"){
+	            	$('.cal-btn').css('display' , 'inline-block');
+	            	return false;
+	            }else{
+	            	$('.cal-btn').css('display' , 'none');
+	            }
+  			});
+		});
+	});
+</script>	   
+					<div class="title">
+				      <ul>
+				        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
+					    <li><span>مدیریت نظرسنجی</span></li>
+				      </ul>
+				      <div class="badboy"></div>
+				  </div>
+                    <div class="Top">                       
+						<center>
+							<form action="?item=pollmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+								<p>جستجو بر اساس {$combobox}</p>
+
+								<p class="search-form">
+									<input type="text" id="date_input_1" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  /> 
+									<img src="./images/cal.png" class="cal-btn" id="date_btn_2" alt="cal-pic">
+							         <script type="text/javascript">
+							          Calendar.setup({
+							            inputField  : "date_input_1",   // id of the input field
+							            button      : "date_btn_2",   // trigger for the calendar (button ID)
+							                ifFormat    : "%Y/%m/%d",       // format of the input field
+							                showsTime   : false,
+							                dateType  : 'jalali',
+							                showOthers  : true,
+							                langNumbers : true,
+							                weekNumbers : true
+							          });
+							        </script>
+									<a href="?item=pollmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=pollmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+								</p>
+								<input type="hidden" name="mark" value="srhnews" /> 
+								{$msgs}
+								{$gridcode} 
+							</form>
+					   </center>
+					</div>
+edit;
+$html = $code;
+}	
 	return $html;
 ?>	
