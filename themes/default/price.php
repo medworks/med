7c7f12310699fee367a52bc2ab7aab4e078af0a1
mojiAm <amjadi.mojtaba@gmail.com>
2013-12-15 11:24:37 -0800

    add price files

--- themes/default/price.php
+++ themes/default/price.php
@@ -1,14 +1,20 @@
 <?php
 
-  	$html.=<<<cd
-    <div class='content'>
+    $html.=<<<cd
+    <div class='content price'>
         <div class='main-box'>
           <h2>تعرفه ها</h2>
           <div class='line'></div>
           <div class='badboy'>
         </div>
-		    <div class='box-right'> 
-  		    
+        <div class='box-right'>
+          <p>لیست قیمتهای مدیا تک به شرح زیر می باشد:</p> 
+          <p><a href="./price/price.pdf" title="دانلود فایل قیمتها به صورت pdf" target="_blank">دانلود فایل قیمتها به صورت pdf</a></p>
+          <p><a href="./price/price.zip" title="دانلود فایل قیمتها به صورت zip" target="_blank">دانلود فایل قیمتها به صورت zip</a></p>
+          <p><a href="./price/price-1.jpg" title="دانلود صفحه 1 قیمتها با پسوند jpg" target="_blank">دانلود صفحه 1 قیمتها با پسوند jpg</a></p>
+          <p><a href="./price/price-2.jpg" title="دانلود صفحه 2 قیمتها با پسوند jpg" target="_blank">دانلود صفحه 2 قیمتها با پسوند jpg</a></p>
+          <p><a href="./price/price-3.jpg" title="دانلود صفحه 3 قیمتها با پسوند jpg" target="_blank">دانلود صفحه 3 قیمتها با پسوند jpg</a></p>
+          <p><a href="./price/price-4.jpg" title="دانلود صفحه 4 قیمتها با پسوند jpg" target="_blank">دانلود صفحه 4 قیمتها با پسوند jpg</a></p>
           <div class='badboy'>
         </div>
     </div>
